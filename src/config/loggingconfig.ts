b5623f60d74aeb6991808ea771d8779f50fad922
ruhaox <1994ep@gmail.com>
2021-05-08 17:47:53 +0800

    Add logging

---
+++ src/config/loggingconfig.ts
@@ -0,0 +1,15 @@
+
+export const LoggingConfig = {
+  level: "debug",
+  perf: false,
+  colors: {
+    error: "red",
+    debug: "blue",
+    warn: "yellow",
+    data: "grey",
+    info: "green",
+    verbose: "cyan",
+    silly: "magenta",
+    custom: "yellow"
+  }
+};
---
+++ src/logging/logger.ts
@@ -0,0 +1,21 @@
+import rTracer from "cls-rtracer";
+import { LoggingConfig } from "../config/loggingconfig";
+import winston from "winston";
+
+const rTracerFormat = winston.format.printf((info) => {
+  const rid = rTracer.id();
+  return rid
+    ? `[${info.level}][${rid}] ${info.message}`
+    : `[${info.level}] ${info.message}`;
+});
+
+winston.addColors(LoggingConfig.colors);
+
+export const logger = winston.createLogger({
+  level: LoggingConfig.level,
+  format: winston.format.combine(
+    winston.format.colorize(),
+    rTracerFormat
+  ),
+  transports: [new winston.transports.Console()]
+});
