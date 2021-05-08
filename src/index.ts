5bbb48a5045579a847a87a8b45c077dd7cff9930
ruhaox <8232572+bigdaddyrus@users.noreply.github.com>
2021-05-08 18:02:37 +0800

    Add logging (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from "express";
 import {Request, Response} from "express";
+import { logger } from "./logging/logger";
 import "reflect-metadata";
 import {createConnection} from "typeorm";
 import {DBConfig} from "./config/dbconfig";
+import bodyParser from "body-parser";
+import rTracer from "cls-rtracer";
 
 createConnection(DBConfig).then(async connection => {
+  logger.debug(`DB connection established with options: ${JSON.stringify(connection.options)}`);
 
   const PORT = 4000;
 
   const app = express();
+  app.use(bodyParser.json());
+  app.use(rTracer.expressMiddleware());
+
   app.listen(PORT, () => {
-    console.log(`>>> Hello: DAAPIServer is running on port ${PORT} <<<`);
+    logger.info(`>>>>> Hello: DAAPIServer is running on port=${PORT} <<<<<`);
   });
 
   app.get("/health", (req: Request, res: Response) => res.send("Serivce is healthy."));
