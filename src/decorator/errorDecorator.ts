9c07d79b02994ebaf6a93f74ce793b77399ff37a
ruhaox <1994ep@gmail.com>
2021-05-09 14:31:08 +0800

    Add error handler and validator

---
+++ src/decorator/errorDecorator.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { ErrorHandler } from "../error/errorHandler";
+import { logger } from "../logging/logger";
+
+export const HandleError = (funcName: string): any => {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    // Save a reference to the original method
+    const originalMethod = descriptor.value;
+
+    // Rewrite original method with try/catch wrapper
+    descriptor.value = function (...args: [Request, Response]) {
+      try {
+        const result = originalMethod.apply(this, args);
+        
+        // Check if method is asynchronous
+        if (result && result instanceof Promise) {
+          // Return promise
+          return result.catch((error: any) => {
+            logger.error(`Encountered problem calling ${funcName}:`, error);
+            ErrorHandler.handle(args[1], error);
+          });
+        }
+
+        // Return actual result
+        return result;
+      } catch(error) {
+        logger.error(`Encountered problem calling ${funcName}:`, error);
+        ErrorHandler.handle(args[1], error);
+      }
+    };
+    return descriptor;
+  };
+};
---
+++ src/error/authError.ts
@@ -0,0 +1,15 @@
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import { BaseError } from "./baseError";
+
+export class AuthError extends BaseError {
+  code: number
+  name: string
+
+  constructor(args: string | undefined){
+    super(args);
+    this.name = ReasonPhrases.UNAUTHORIZED;
+    this.code = StatusCodes.UNAUTHORIZED;
+  }
+}
+
+
