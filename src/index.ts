91e93229d5d55d05501c00322843a20f48cf78fd
Yicheng Liang <yichengliang@Yichengs-MacBook-Pro.local>
2021-06-05 17:38:05 +0800

    use development instead of local

--- src/index.ts
+++ src/index.ts
@@ -22,17 +22,20 @@ createConnection(DBConfig).then(async connection => {
   app.use(rTracer.expressMiddleware());
   const router = Router();
 
-  if (process.env.APP_ENV !== "local") {
+  if (process.env.APP_ENV !== "development") {
     let options = {
       key: fs.readFileSync("/home/admin/app/src/5752003_www.integ.lt.pbrick.cn.key"),
       cert: fs.readFileSync("/home/admin/app/src/5752003_www.integ.lt.pbrick.cn.pem")
     };
-    https.createServer(options, app).listen(443);
-  }
-
-  app.listen(PORT, () => {
+    https.createServer(options, app).listen(PORT);
     logger.info(`>>>>> Haven't felt like this in a longtime=${PORT} <<<<<`);
-  });
+
+  } else {
+    app.listen(PORT, () => {
+      logger.info(`>>>>> Haven't felt like this in a longtime=${PORT} <<<<<`);
+    });
+  }
+  
   app.use("/", router);
   app.get("/health", (req: Request, res: Response) => res.send("Serivce is healthy."));
   router.use("/v1", v1router);
