1b1d210d39cfededf4edd8a31abcad05050b9cf4
ruhaox <1994ep@gmail.com>
2021-06-18 17:27:41 +0800

    remove unused

--- src/payrouter.ts
+++ src/payrouter.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { PaymentController } from "./controller/payment";
-// import bodyParser from "body-parser";
 const bodyParser = require("body-parser");
 require("body-parser-xml")(bodyParser);
 
