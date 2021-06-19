36dc71cb541a0812b0373d39a939548bc2b330f0
Yicheng Liang <yichengliang@Yichengs-MacBook-Pro.local>
2021-06-19 18:00:01 +0800

    added pagination

--- src/controller/userRelation.ts
+++ src/controller/userRelation.ts
@@ -1,13 +1,15 @@
 import { Request, Response, NextFunction } from "express";
-import { getRepository } from "typeorm";
+import { getRepository, OrderByCondition } from "typeorm";
 import { HandleError } from "../decorator/errorDecorator";
 import { ResourceNotFoundError } from "../error/notfoundError";
 import { Users } from "../entities/Users";
 import { UserRelations } from "../entities/UserRelations";
 import { Items } from "../entities/Items";
-
 import { logger } from "../logging/logger";
 import { ListingStatus } from "../entities/Items";
+import { getOrderByConditions } from "./helper/orderByHelper";
+import { getPaginationLinks, getPaginationParams } from "./helper/paginationHelper";
+const ITEMS_DEFAULT_SORT_BY:OrderByCondition = { "items.updatedtime":"DESC" };
 
 export class UserRelationController {
 
@@ -126,12 +128,16 @@ export class UserRelationController {
   }
 
 
-  @HandleError("getUserFollowingItems") // TODO pagination
+  @HandleError("getUserFollowingItems") 
+
   static async getUserFollowingItems(req: Request, res: Response): Promise<void> {
     const userId = req.params.id;
     const userRelationRepo = await getRepository(UserRelations);
+    const sorts = req.query.sort;
+    const orderBy = getOrderByConditions(sorts, ITEMS_DEFAULT_SORT_BY);
+    const [pageNumber, skipSize, pageSize] = getPaginationParams(req.query.page);
 
-    const followingUser = await userRelationRepo.createQueryBuilder("userRelations")
+    const followingUsers = await userRelationRepo.createQueryBuilder("userRelations")
       .leftJoinAndSelect("userRelations.follower", "follower")
       .leftJoinAndSelect("userRelations.followee", "followee")
       .leftJoinAndSelect("followee.shops", "shops")
@@ -143,17 +149,21 @@ export class UserRelationController {
       ])
       .getMany();
 
-    const followingUserIds = followingUser.map(user => user.followee.shops[0].id); // currently one user can only has one shop
+    const followingUserIds = followingUsers.map(user => user.followee.shops[0].id); // currently one user can only has one shop
     const userFollowingItems = await getRepository(Items)
       .createQueryBuilder("items")
       .leftJoinAndSelect("items.shop", "shop")
       .leftJoinAndSelect("shop.owner", "users")
       .where("items.shopId IN (:...ids)", { ids: followingUserIds })
       .select(["items", "shop.name", "shop.id", "shop.introduction", "shop.logoUrl", "shop.location", "users.id", "users.username"])
+      .orderBy(orderBy)
+      .skip(skipSize)
+      .take(pageSize)
       .getMany();
 
     res.send({
-      data: userFollowingItems
+      data: userFollowingItems,
+      links: getPaginationLinks(req, pageNumber, pageSize)
     });
   }
 
