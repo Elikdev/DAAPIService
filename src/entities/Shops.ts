e87aca9039d77eeb8bcbdb2807c47ba4cf4f15f7
Fangzhou He <fangzhouhe1995@gmail.com>
2021-06-22 14:52:27 +0800

    add customerServiceUrl for Shop entities

--- src/entities/Shops.ts
+++ src/entities/Shops.ts
@@ -46,6 +46,9 @@ export class Shops extends BaseEntity {
   logoUrl: string;
 
   @Column("character varying", {nullable: true })
+  customerServiceUrl: string;
+
+  @Column("character varying", {nullable: true })
   location: string;
 
   @Column("float", { default: 0.0})
