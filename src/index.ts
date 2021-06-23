71c3c9cbc0ab75839ffbdf8b1f6b44463f5ec1d9
ruhaox <1994ep@gmail.com>
2021-06-23 16:15:10 +0800

    use http and https

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,12 @@ import rTracer from "cls-rtracer";
 import { v1router } from "./v1router";
 import * as dotenv from "dotenv";
 import https from "https";
+import http from "http";
 import { getServerOptions } from "./config/serverconfig";
 import { payrouter } from "./payrouter";
 
-const PORT = 4000;
+const HTTPS_PORT = 4001;
+const HTTP_PORT = 4000;
 const DBConfig = getDBConfig();
 const httpsOptions = getServerOptions();
 
@@ -24,11 +26,11 @@ createConnection(DBConfig).then(async connection => {
   const router = Router();
 
   if (httpsOptions) {
-    https.createServer(httpsOptions, app).listen(PORT);
-  } else {
-    app.listen(PORT);
+    https.createServer(httpsOptions, app).listen(HTTPS_PORT);
   }
-  logger.info(`>>>>> Haven't felt like this in a longtime=${PORT} <<<<<`);
+  http.createServer(app).listen(HTTP_PORT);
+  
+  logger.info(`>>>>> Haven't felt like this in a longtime=${HTTPS_PORT} <<<<<`);
   
   app.use("/", router);
   app.get("/health", (req: Request, res: Response) => res.send("Serivce is healthy."));
