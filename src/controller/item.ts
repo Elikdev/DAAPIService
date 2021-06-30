d77a72b0429c731fa4867b0d08b1b9f930ad5f7a
Yicheng Liang <yichengliang@Yichengs-MacBook-Pro.local>
2021-06-30 12:38:44 +0800

    getMany

--- src/controller/item.ts
+++ src/controller/item.ts
@@ -27,14 +27,14 @@ export class ItemController {
     logger.debug("OrderBy: " + JSON.stringify(orderBy));
     const items = await itemRepo
       .createQueryBuilder("item")
-      .innerJoin("item.shop", "shops")
+      .leftJoin("item.shop", "shops")
       // .leftJoin("item.shop", "Shops")
       .where("shops.isSuspended = :isSuspended", { isSuspended: false })
       .andWhere("item.status = :new", { new: ListingStatus.NEW })
       // .orderBy(orderBy)
       .skip(skipSize)
       .take(pageSize)
-      .getOne();
+      .getMany();
 
     res.send({
       data: items,
