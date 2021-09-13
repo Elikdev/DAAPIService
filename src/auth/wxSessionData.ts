ae4d7c3ba861acf2646eef35ab16c5c4ea8b512c
OrangeChickenss <lyc2861131@gmail.com>
2021-09-13 23:19:23 +0800

    app wx login

--- src/auth/wxSessionData.ts
+++ src/auth/wxSessionData.ts
@@ -21,3 +21,34 @@ export const getSessionData = async (wxCode: string): Promise<any> => {
 
   return sessionData;
 };
+
+
+export const getUserInfo = async (wxCode: string): Promise<any> => {
+  const params =  {
+    appid: process.env.WX_APP_APP_ID,
+    secret: process.env.WX_APP_APP_SECRET,
+    code: wxCode,
+    grant_type: "authorization_code"
+  };
+  const queryString = qs.stringify(params);
+  const wxurl = `https://api.weixin.qq.com/sns/oauth2/access_token?${queryString}`;
+
+  return await axios.post(wxurl).then(async function (res) {
+    const userParams = {
+      access_token: res.data.access_token,
+      openid: res.data.openid,
+      lang: "zh_CN"
+    }
+    const userInfoQueryString = qs.stringify(userParams);
+    const uerinfoUrl = `https://api.weixin.qq.com/sns/userinfo?${userInfoQueryString}`;
+    const userInfoResponse = await axios.get(uerinfoUrl).then(function (res) {
+      return res.data;
+    });
+    return userInfoResponse
+
+  }).catch(function (err) {
+    console.error(err);
+  });
+
+  
+};
--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,70 +1,138 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import { JwtHelper } from "../auth/jwt";
-import { getSessionData } from "../auth/wxSessionData";
+import { getSessionData, getUserInfo} from "../auth/wxSessionData";
 import { HandleError } from "../decorator/errorDecorator";
 import { Users } from "../entities/Users";
 import { AuthError } from "../error/authError";
 import { ResourceNotFoundError } from "../error/notfoundError";
 import { logger } from "../logging/logger";
 import { RequestValidator } from "../validator/requestValidator";
-import { signUpSchema, updateUserSchema } from "../validator/schemas";
+import { signUpSchema, updateUserSchema, appSignUpSchema } from "../validator/schemas";
 import { Decode } from "./helper/wxDecode";
 import { Constants } from "../config/constants";
+import { Platform } from "../entities/Users";
 
 export class UserController {
 
   @HandleError("signUp")
   static async signUp(req: Request, res: Response): Promise<void> {
-    const validator = new RequestValidator(signUpSchema);
     const userData = req.body.data;
-    const code = userData.code;
-    const sessionData = await getSessionData(code);
-    validator.validate(userData);
-    const encryptedData = userData.encryptedData;
-    const iv  = userData.iv;
-    const encryptedDataDecoder = new Decode(sessionData.session_key);
+    const platform = userData.platform
     const userRepo = getRepository(Users);
-    const openId = sessionData.openid;
 
-    logger.info("Decrypting user data with session_key:" + sessionData.session_key);
-    const userInfo = encryptedDataDecoder.decryptData(encryptedData, iv);
-    let user = await userRepo.createQueryBuilder("user")
-      .where("user.openId = :openId", { openId: openId })
-      .leftJoinAndSelect("user.shops", "shops")
-      .leftJoinAndSelect("user.itemLikes", "itemLikes")
-      .leftJoinAndSelect("itemLikes.item", "likedItem")
-      .leftJoinAndSelect("user.itemSaves", "itemSaves")
-      .leftJoinAndSelect("itemSaves.item", "savedItem")
-      .loadRelationCountAndMap("user.itemLikesCount", "user.itemLikes")
-      .loadRelationCountAndMap("user.itemSavesCount", "user.itemSaves")
-      .getOne();
-
-    let newUser = false;
-    if (!user) {
-      logger.info("Creating new user record.");
-      userData.openId = openId;
-      userData.mobilePrefix = userInfo.countryCode;
-      userData.mobile = userInfo.phoneNumber;
-      userData.username = userInfo.phoneNumber;
-      userData.role = Constants.SHOPPER;
-      user = await userRepo.save(userData);
+    if (platform === Platform.APP) { // login from app
+      const validator = new RequestValidator(appSignUpSchema);
+      validator.validate(userData);
+      const code = userData.code
+      const userInfo = await getUserInfo(code)
+      let user = await userRepo.createQueryBuilder("user")
+        .where("user.unionId = :unionId", { unionId: userInfo.unionid })
+        .leftJoinAndSelect("user.shops", "shops")
+        .leftJoinAndSelect("user.itemLikes", "itemLikes")
+        .leftJoinAndSelect("itemLikes.item", "likedItem")
+        .leftJoinAndSelect("user.itemSaves", "itemSaves")
+        .leftJoinAndSelect("itemSaves.item", "savedItem")
+        .loadRelationCountAndMap("user.itemLikesCount", "user.itemLikes")
+        .loadRelationCountAndMap("user.itemSavesCount", "user.itemSaves")
+        .getOne();
+      let newUser = false;
       if (!user) {
-        throw new AuthError("Failed to create user.");
+        logger.info("Creating new user record.");
+        userData.openId = userInfo.openid;
+        userData.username = userInfo.nickname;
+        userData.role = Constants.SHOPPER;
+        userData.unionId = userInfo.unionid;
+        userData.avatarUrl = userInfo.headimgurl;
+        userData.sex = userInfo.sex;
+        userData.platform = Platform.APP;
+        user = await userRepo.save(userData);
+        if (!user) {
+          throw new AuthError("Failed to create user.");
+        }
+        newUser = true;
       }
-      newUser = true;
+
+      const payload = {
+        customerId: user.id
+      };
+
+      const accessToken = JwtHelper.sign(payload);
+
+      res.send({
+        loginToken: accessToken,
+        userInfo: user,
+        newUser: newUser
+      });
+
+    } else {
+      const validator = new RequestValidator(signUpSchema);
+
+      const code = userData.code;
+      const sessionData = await getSessionData(code);
+
+      validator.validate(userData);
+      const encryptedData = userData.encryptedData;
+      const iv  = userData.iv;
+      const encryptedDataDecoder = new Decode(sessionData.session_key);
+      const openId = sessionData.openid;
+      const unionId = sessionData.unionid;
+
+      logger.info("Decrypting user data with session_key:" + sessionData.session_key);
+      const userInfo = encryptedDataDecoder.decryptData(encryptedData, iv);
+      let user = await userRepo.createQueryBuilder("user") // check if user created through app first as app sign in only stores unionid 
+        .where("user.unionId = :unionId", { unionId: unionId })
+        .leftJoinAndSelect("user.shops", "shops")
+        .leftJoinAndSelect("user.itemLikes", "itemLikes")
+        .leftJoinAndSelect("itemLikes.item", "likedItem")
+        .leftJoinAndSelect("user.itemSaves", "itemSaves")
+        .leftJoinAndSelect("itemSaves.item", "savedItem")
+        .loadRelationCountAndMap("user.itemLikesCount", "user.itemLikes")
+        .loadRelationCountAndMap("user.itemSavesCount", "user.itemSaves")
+        .getOne();
+      
+      let newUser = false;
+      if (!user) { 
+        user = await userRepo.createQueryBuilder("user")
+        .where("user.openId = :openId", { openId: openId })
+        .leftJoinAndSelect("user.shops", "shops")
+        .leftJoinAndSelect("user.itemLikes", "itemLikes")
+        .leftJoinAndSelect("itemLikes.item", "likedItem")
+        .leftJoinAndSelect("user.itemSaves", "itemSaves")
+        .leftJoinAndSelect("itemSaves.item", "savedItem")
+        .loadRelationCountAndMap("user.itemLikesCount", "user.itemLikes")
+        .loadRelationCountAndMap("user.itemSavesCount", "user.itemSaves")
+        .getOne();
+        if (!user) {
+          logger.info("Creating new user record.");
+          userData.openId = openId;
+          userData.mobilePrefix = userInfo.countryCode;
+          userData.mobile = userInfo.phoneNumber;
+          userData.username = userInfo.phoneNumber;
+          userData.role = Constants.SHOPPER;
+          userData.unionId = unionId;
+          user = await userRepo.save(userData);
+        }
+        if (!user) {
+          throw new AuthError("Failed to create user.");
+        }
+        newUser = true;
+      }
+      user.unionId = unionId;
+      user.platform = Platform.MINIPROGRAM;
+      await user.save() // back fill union Id
+      const payload = {
+        customerId: user.id
+      };
+
+      const accessToken = JwtHelper.sign(payload);
+
+      res.send({
+        loginToken: accessToken,
+        userInfo: user,
+        newUser: newUser
+      });
     }
-    const payload = {
-      customerId: user.id
-    };
-
-    const accessToken = JwtHelper.sign(payload);
-
-    res.send({
-      loginToken: accessToken,
-      userInfo: user,
-      newUser: newUser
-    });
 
   }
 
