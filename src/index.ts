39412ad5da55dca86e3935dc18f4661958737ab1
OrangeChickenss <lyc2861131@gmail.com>
2022-01-12 19:04:49 +0800

    allowing * for socket server

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,11 @@ createConnection(DBConfig)
     } else {
       const server = http.createServer(app);
       server.listen(PORT, () => console.log(`server running on port ${PORT}`));
-      const io = new socketio.Server(server);
+      const io = new socketio.Server(server, {
+        cors: {
+          origin: "*",
+        },
+      });
       io.on("connection", (socket) => {
         logger.info("new ws connection");
         // Listen for Join room
